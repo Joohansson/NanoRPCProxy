cc6678abd93e0a9aa31436d4e1d44dd708095e87
Joohansson <d84@outlook.com>
2020-04-11 10:03:54 +0200

    Additional client command

--- src/demo_clients/client.py
+++ src/demo_clients/client.py
@@ -4,6 +4,7 @@ import argparse
 
 commands = [
         {"action":"block_count"},
+        {"action":"account_info","account":"nano_3t6k35gi95xu6tergt6p69ck76ogmitsa8mnijtpxm9fkcm736xtoncuohr3"},
         {"action":"account_history", "account":"nano_3cpz7oh9qr5b7obbcb5867omqf8esix4sdd5w6mh8kkknamjgbnwrimxsaaf", "count":"20"},
         {"action":"active_difficulty"},
         {"action":"block_info","json_block":"true","hash":"87434F8041869A01C8F6F263B87972D7BA443A72E0A97D7A3FD0CCC2358FD6F9"},
@@ -12,8 +13,8 @@ commands = [
     ]
 
 # Parse argument --c [int] to call different commands
-parser = argparse.ArgumentParser(description="Call proxy")
-parser.add_argument("--c", default=1, type=int, choices=[1, 2, 3, 4, 5, 6], required=True, help="The action to call")
+parser = argparse.ArgumentParser(description="Call proxy server")
+parser.add_argument("--c", default=1, type=int, choices=[1, 2, 3, 4, 5, 6, 7], required=True, help="The action to call")
 args = parser.parse_args()
 command = commands[int(args.c)-1]
 
