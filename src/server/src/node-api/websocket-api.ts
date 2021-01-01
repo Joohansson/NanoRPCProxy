536acb7d505790134ff443f8a51b2bf37efd6d40
Kjetil Mehl <kjetil@mehl.no>
2021-01-02 00:47:50 +0100

    Moves to websocket file

---
+++ src/server/src/node-api/websocket-api.ts
@@ -0,0 +1,40 @@
+type WSTopic = 'confirmation'
+type WSAction = 'subscribe' | 'unsubscribe'
+
+interface WSNodeSubscribe {
+    action: WSAction
+    topic: WSTopic
+    ack: boolean
+    options: {
+        all_local_accounts: boolean
+        accounts: string[]
+    }
+}
+interface WSNodeReceive {
+    topic: WSTopic
+    message: {
+        account: string
+        block: {
+            link_as_account: string
+        }
+    }
+    ack: WSAction
+}
+
+interface WSMessage {
+    topic: WSTopic
+    action: WSAction
+    options: {
+        accounts: string[]
+    }
+    id: string
+}
+
+interface WSError {
+    error: string
+}
+
+interface WSSubscribe {
+    ack: WSAction
+    id: string
+}
--- src/server/src/proxy.ts
+++ src/server/src/proxy.ts
@@ -1026,47 +1026,6 @@ if (settings.use_https) {
   }
 }
 
-type WSTopic = 'confirmation'
-type WSAction = 'subscribe' | 'unsubscribe'
-
-interface WSNodeSubscribe {
-  action: WSAction
-  topic: WSTopic
-  ack: boolean
-  options: {
-    all_local_accounts: boolean
-    accounts: string[]
-  }
-}
-interface WSNodeReceive {
-  topic: WSTopic
-  message: {
-    account: string
-    block: {
-      link_as_account: string
-    }
-  }
-  ack: WSAction
-}
-
-interface WSMessage {
-  topic: WSTopic
-  action: WSAction
-  options: {
-    accounts: string[]
-  }
-  id: string
-}
-
-interface WSError {
-  error: string
-}
-
-interface WSSubscribe {
-  ack: WSAction
-  id: string
-}
-
 // WEBSOCKET SERVER
 //---------------------
 if (settings.use_websocket) {
